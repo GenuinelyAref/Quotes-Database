eb7997ba5060fd8085fd8d084787c97db4ecaa1a
GenuinelyAref <osmana69529@masseyhigh.school.nz>
2021-12-26 12:55:04 +1300

    create essential files
    create database config file, that allows database access to the server.
    
    create functions file, that 'sanitises' data, utilises autocomplete and allows IDs to be created as needed (except the primary key ID, which is created automatically by adding a data row into the database)

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+
+ini_set('display_errors', 'On'); // change to 'Off' when live
+
+define ("DB_NAME", "osmana69529_L2_Apps");
+define ("DB_USERNAME", "_osmana69529");
+define ("DB_PASSWORD", "eiTpwreg82OZoWh1");
+define ("DB_HOST", "localhost");
+
+?>
