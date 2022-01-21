547676bcae63a6f012f54652356750ba9f80bde5
GenuinelyAref <osmana69529@masseyhigh.school.nz>
2022-01-22 00:04:06 +1300

    limit 'recent quotes' page results to 10
    (also removed line break at the end of the SQL for easier reading)

--- content/recent.php
+++ content/recent.php
@@ -4,8 +4,7 @@
 
 $find_sql = "SELECT * FROM `quotes`
 JOIN `author` ON (`author`.`Author_ID` = `quotes`.`Author_ID`)
-ORDER BY `quotes`.`ID` DESC
-";
+ORDER BY `quotes`.`ID` DESC LIMIT 10";
 $find_query = mysqli_query($dbconnect, $find_sql);
 $find_rs = mysqli_fetch_assoc($find_query);
 
