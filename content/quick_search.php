2623303a724dc2c2fd49b4fde509427c894cfc40
GenuinelyAref <osmana69529@masseyhigh.school.nz>
2022-01-22 00:03:21 +1300

    update search logic
    search algorithm now searches by the keyword in the 'quotes' data column

--- content/quick_search.php
+++ content/quick_search.php
@@ -22,6 +22,7 @@ JOIN `author` ON (`author`.`Author_ID` = `quotes`.`Author_ID`)
 WHERE `Last` LIKE '%$quick_find%'
 OR `Middle` LIKE '%$quick_find%'
 OR `First` LIKE '%$quick_find%'
+OR `Quote` LIKE '%$quick_find%'
 OR `Subject1_ID` = $subject_ID
 OR `Subject2_ID` = $subject_ID
 OR `Subject3_ID` = $subject_ID
