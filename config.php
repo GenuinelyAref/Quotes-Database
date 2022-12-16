51bd0c16a095bb08f70bfaab6379f5210ac57f02
GenuinelyAref <osmana69529@masseyhigh.school.nz>
2022-12-16 23:35:38 +1300

    change db directory

--- config.php
+++ config.php
@@ -2,7 +2,7 @@
 
 ini_set('display_errors', 'On'); // change to 'Off' when live
 
-define ("DB_NAME", "osmana69529_L2_Apps");
+define ("DB_NAME", "osmana69529_L3_Quotes");
 define ("DB_USERNAME", "_osmana69529");
 define ("DB_PASSWORD", "eiTpwreg82OZoWh1");
 define ("DB_HOST", "localhost");
